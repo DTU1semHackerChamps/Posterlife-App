8700e268944e8160bc4a81a45bf66bf23f883999
Emil Trangeled <emilbfh@hotmail.com>
2021-10-21 17:31:05 +0200

    Added login- and register-activity and  navigation between them and MainActivity.kt

---
+++ app/src/main/java/com/example/posterlifeapp/RegisterActivity.kt
@@ -0,0 +1,18 @@
+package com.example.posterlifeapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        backBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
