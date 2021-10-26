42fe94c5e07c19635047d3f16551d24dbe59b954
Emil Trangeled <emilbfh@hotmail.com>
2021-10-26 11:52:37 +0200

    Bottom nav bar added, with 3 tabs

--- app/src/main/java/com/example/posterlifeapp/MainActivity2.kt
+++ app/src/main/java/com/example/posterlifeapp/MainActivity2.kt
@@ -4,15 +4,18 @@ import android.os.Bundle
 import android.view.Surface
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.posterlifeapp.ui.theme.PosterLifeAppTheme
 import org.w3c.dom.Text
@@ -23,33 +26,33 @@ class MainActivity2 : ComponentActivity() {
         setContent {
             PosterLifeAppTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+//                Surface(color = MaterialTheme.colors.background) {
+//                    Greeting("Android")
+//                }
+                MainScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun MainScreen() {
+    Scaffold(
+        topBar = { TopBar()},
+        bottomBar = { BottomNaigationBar()}) {
 
-@Composable
-fun TopBar() {
-    TopAppBar(
-        title = { Text(text = stringResource(id = R.string.app_name))},
-        backgroundColor = colorResource(id = R.color.design_default_color_primary),
-        contentColor = Color.White
-
-    )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun TopBarPreview() {
-    TopBar()
+fun MainScreenPreview(){
+    MainScreen()
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
@@ -59,3 +62,66 @@ fun DefaultPreview() {
         Greeting("Android")
     }
 }
+
+@Composable
+fun TopBar() {
+    TopAppBar(
+        backgroundColor = colorResource(id = R.color.design_default_color_primary),
+        contentColor = Color.White,
+        modifier = Modifier.fillMaxWidth()
+    ){
+        Box(modifier = Modifier.height(32.dp)){
+
+            Row(Modifier.fillMaxSize()) {
+
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    maxLines = 1,
+                    text = stringResource(id = R.string.app_name),
+                    fontSize = 24.sp
+                )
+
+            }
+        }
+
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TopBarPreview() {
+    TopBar()
+}
+
+@Composable
+fun BottomNaigationBar(){
+    val items = listOf(
+        NavigationItem.Inspiration,
+        NavigationItem.Profile,
+        NavigationItem.Share
+    )
+    BottomNavigation(
+        backgroundColor = colorResource(id = R.color.design_default_color_primary),
+        contentColor = Color.White
+    ) {
+        items.forEach{ item ->
+            BottomNavigationItem(
+                icon = {Icon(painterResource(id = item.icon), contentDescription = item.title)},
+                label = { Text(text = item.title)},
+                selectedContentColor = Color.White,
+                unselectedContentColor = Color.White.copy(0.4f),
+                alwaysShowLabel = true,
+                selected = false,
+                onClick = { /*TODO*/ })
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BottomNavigationBarPreview() {
+    BottomNaigationBar()
+}
+
---
+++ app/src/main/java/com/example/posterlifeapp/NavigationItem.kt
@@ -0,0 +1,7 @@
+package com.example.posterlifeapp
+
+sealed class NavigationItem(var route: String, var icon: Int, var title: String){
+    object Inspiration : NavigationItem("inspiration", R.drawable.ic_lightbulb_black_24dp, "Inspiration")
+    object Profile : NavigationItem("profile", R.drawable.ic_person_black_24dp, "Profile")
+    object Share : NavigationItem("share", R.drawable.ic_share_black_24dp, "Share")
+}
