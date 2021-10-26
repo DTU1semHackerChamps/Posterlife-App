5c6dbe2528a3c213a11ed5973d2541be17ae7c46
harzzek <39692510+harzzek@users.noreply.github.com>
2021-10-26 18:33:21 +0200

    Lagt dependencies

--- app/src/main/java/com/example/posterlifeapp/ContentView.kt
+++ app/src/main/java/com/example/posterlifeapp/ContentView.kt
@@ -1,19 +1,20 @@
 package com.example.posterlifeapp
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
+import androidx.compose.foundation.Image
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 class ContentView {
@@ -71,20 +72,27 @@ fun ProfileScreenPreview(){
 
 @Composable
 fun ShareScreen(){
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = Color.Blue)
-            .wrapContentSize(Alignment.Center)
-    ) {
-        Text(
-            text = "Share View",
-            fontWeight = FontWeight.Bold,
-            color = Color.White,
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 25.sp
-        )
+    LazyColumn(
+        modifier = Modifier.fillMaxWidth(),
+        contentPadding = PaddingValues(horizontal = 4.dp, vertical = 8.dp),
+        verticalArrangement = Arrangement.spacedBy(2.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+        
+        items(5) {
+            Spacer(modifier = Modifier.padding(14.dp))
+            Row (modifier = Modifier.height(50.dp)){
+                Image(
+                    painter = painterResource(id = R.drawable.ic_share_black_24dp),
+                    contentDescription = "",
+                )
+                Spacer(
+                    modifier = Modifier.padding(8.dp)
+                )
+                Text(text = "Item", fontSize = 20.sp)
+
+            }
+        }
     }
 }
 
