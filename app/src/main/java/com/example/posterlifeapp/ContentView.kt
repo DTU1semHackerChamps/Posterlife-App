1194e7483ee4e81f805e71875a80aef50d61e33f
harzzek <39692510+harzzek@users.noreply.github.com>
2021-10-27 11:34:32 +0200

    Implementeret layout for share

--- app/src/main/java/com/example/posterlifeapp/ContentView.kt
+++ app/src/main/java/com/example/posterlifeapp/ContentView.kt
@@ -9,6 +9,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.foundation.Image
+import androidx.compose.material.Divider
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -16,6 +18,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 
 class ContentView {
 }
@@ -72,28 +75,56 @@ fun ProfileScreenPreview(){
 
 @Composable
 fun ShareScreen(){
-    LazyColumn(
-        modifier = Modifier.fillMaxWidth(),
-        contentPadding = PaddingValues(horizontal = 4.dp, vertical = 8.dp),
-        verticalArrangement = Arrangement.spacedBy(2.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-        
-        items(5) {
-            Spacer(modifier = Modifier.padding(14.dp))
-            Row (modifier = Modifier.height(50.dp)){
-                Image(
-                    painter = painterResource(id = R.drawable.ic_share_black_24dp),
-                    contentDescription = "",
-                )
-                Spacer(
-                    modifier = Modifier.padding(8.dp)
-                )
-                Text(text = "Item", fontSize = 20.sp)
 
-            }
+    val itemsIC = listOf(
+        SocialMediaItem.FacebookIC,
+        SocialMediaItem.TwitterIC,
+        SocialMediaItem.Pinterest,
+        SocialMediaItem.InstagramIC,
+        SocialMediaItem.Email
+    )
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 10.dp),
+        verticalArrangement = Arrangement.spacedBy(15.dp),
+        horizontalAlignment = Alignment.Start
+        ) {
+
+        Divider(
+            thickness = 2.dp,
+            color = Color.Gray
+        )
+        itemsIC.forEach{ item ->
+
+            SocialList(id = item.icon, name = item.title )
+            Divider(
+                thickness = 2.dp,
+                color = Color.Gray
+            )
         }
+
     }
+}
+@Composable
+fun SocialList(id: Int, name: String)
+{
+
+    Row (modifier = Modifier.height(40.dp)
+        .padding(start = 120.dp)
+    ) {
+        Image(
+            painter = painterResource(id = id),
+            contentDescription = name,
+            modifier = Modifier.size(40.dp)
+        )
+        Spacer(
+            modifier = Modifier.padding(8.dp)
+        )
+        Text(text = name, fontSize = 25.sp)
+    }
+
 }
 
 @Preview(showBackground = true)
---
+++ app/src/main/java/com/example/posterlifeapp/SocialMediaItem.kt
@@ -0,0 +1,11 @@
+package com.example.posterlifeapp
+
+sealed class SocialMediaItem(var route: String, var icon: Int, var title: String)
+{
+    object FacebookIC: NavigationItem("Facebook", R.drawable.ic_iconmonstr_facebook_4, "Facebook")
+    object TwitterIC: NavigationItem("Twitter", R.drawable.ic_iconmonstr_twitter_1, "Twitter")
+    object InstagramIC: NavigationItem("Instagram", R.drawable.ic_iconmonstr_instagram_11, "Instagram")
+    object Pinterest: NavigationItem("Pinterest", R.drawable.ic_iconmonstr_pinterest_1, "Pinterest")
+    object Email: NavigationItem("Email", R.drawable.ic_iconmonstr_email_2, "Email")
+
+}
