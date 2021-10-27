8fa5bd04f49ecdbe26037013f846707fbf88843c
Magnus <s194056@student.dtu.dk>
2021-10-27 05:24:53 +0200

    Some camera changes added

--- app/src/main/java/com/example/posterlifeapp/MainActivity2.kt
+++ app/src/main/java/com/example/posterlifeapp/MainActivity2.kt
@@ -1,6 +1,7 @@
 package com.example.posterlifeapp
 
 import android.os.Bundle
+
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
@@ -24,7 +25,11 @@ import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.posterlifeapp.ui.theme.PosterLifeAppTheme
 
+typealias LumaListener = (luma: Double) -> Unit
+
+
 class MainActivity2 : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -151,6 +156,7 @@ fun Navigation(navController: NavHostController) {
     NavHost(navController, startDestination = NavigationItem.Inspiration.route) {
         composable(NavigationItem.Inspiration.route) {
             InspirationScreen()
+
         }
         composable(NavigationItem.Profile.route) {
             ProfileScreen()
