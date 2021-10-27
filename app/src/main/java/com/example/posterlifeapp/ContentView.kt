3576bbff816bc06dbb539407898213844725872f
Emil Trangeled <emilbfh@hotmail.com>
2021-10-27 11:34:13 +0200

    Added scrollable screen in inspiration with posters

--- app/src/main/java/com/example/posterlifeapp/ContentView.kt
+++ app/src/main/java/com/example/posterlifeapp/ContentView.kt
@@ -1,41 +1,67 @@
 package com.example.posterlifeapp
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 class ContentView {
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun InspirationScreen(){
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = Color.Green)
-            .wrapContentSize(Alignment.Center)
-    ) {
-        Text(
-            text = "Inspiration View",
-            fontWeight = FontWeight.Bold,
-            color = Color.White,
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 25.sp
-        )
+    val posters = PosterList()
+    LazyVerticalGrid(
+        cells = GridCells.Fixed(2),
+        modifier = Modifier.padding(bottom = 33.dp)
+    ){
+        items(posters.size) { index ->
+            SinglePicAndText(imageID = posters[index].poster, title = posters[index].title)
+        }
     }
+
+
+}
+
+@Composable
+fun SinglePicAndText(imageID: Int, title: String) {
+    val image: Painter = painterResource(imageID)
+    Column(modifier = Modifier.padding(4.dp)) {
+        Image(
+            painter = image,
+            contentDescription = title,
+//            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+        Row() {
+            Text(
+                text = title,
+                fontWeight = FontWeight.Bold,
+                color = Color.Black,
+                textAlign = TextAlign.Center,
+                fontSize = 25.sp
+            )
+        }
+
+    }
+
+
 }
 
 @Preview(showBackground = true)
