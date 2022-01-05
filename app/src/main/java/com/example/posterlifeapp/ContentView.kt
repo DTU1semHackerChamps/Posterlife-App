786507ff3455377e4f7afe4190188a070d643685
harzzek <39692510+harzzek@users.noreply.github.com>
2022-01-05 13:07:50 +0100

    Json paarse

--- app/src/main/java/com/example/posterlifeapp/ContentView.kt
+++ app/src/main/java/com/example/posterlifeapp/ContentView.kt
@@ -1,6 +1,10 @@
 package com.example.posterlifeapp
 
 import android.content.res.AssetManager
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import android.nfc.Tag
+import android.util.Log
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
@@ -11,12 +15,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.graphics.drawable.toDrawable
+import coil.Coil
+import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
 import com.example.posterlifeapp.Repositories.InspirationRepository
 import com.example.posterlifeapp.Repositories.Utils
@@ -24,30 +35,31 @@ import com.example.posterlifeapp.Repositories.Utils
 class ContentView {
 }
 
-@OptIn(ExperimentalFoundationApi::class)
+
+@ExperimentalFoundationApi
 @Composable
 fun InspirationScreen(assets : AssetManager){
-    val posters = PosterList()
-    val pop = Utils(assets)
-
-    pop.postersFromAPI()
+    val util = Utils(assets)
+    val posters = util.postersFromAPI()
 
     LazyVerticalGrid(
         cells = GridCells.Fixed(2),
         modifier = Modifier.padding(bottom = 50.dp)
     ){
         items(posters.size) { index ->
-            SinglePicAndText(imageID = posters[index].poster, title = posters[index].title)
+            SinglePicAndText(imageID = util.posterBitMap(posters[index].imageUrl),
+                title = posters[index].title)
         }
     }
 }
 
+
 @Composable
-fun SinglePicAndText(imageID: Int, title: String) {
+fun SinglePicAndText(imageID: Bitmap, title: String) {
 
     Column(modifier = Modifier.padding(4.dp)) {
         Image(
-            painter = rememberImagePainter(imageID),
+            bitmap = imageID.asImageBitmap(),
             contentDescription = title,
 //            contentScale = ContentScale.Crop,
             modifier = Modifier.fillMaxSize()
--- app/src/main/java/com/example/posterlifeapp/Repositories/Utils.kt
+++ app/src/main/java/com/example/posterlifeapp/Repositories/Utils.kt
@@ -2,24 +2,40 @@ package com.example.posterlifeapp.Repositories
 
 import android.content.ContentValues.TAG
 import android.content.res.AssetManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.util.Log
+import androidx.compose.ui.graphics.ImageBitmap
 import com.example.posterlifeapp.model.Poster
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import org.json.JSONArray
 import org.json.JSONObject
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.URL
+import java.net.URLConnection
 import java.util.*
 
 class Utils( val assets : AssetManager) {
 
-    fun postersFromAPI()
+    fun postersFromAPI() : List<Poster>
     {
         val jsonString = assets.open("posterlife.json")
-        jsonString.read()
+        val size = jsonString.available()
+        val foo = ByteArray(size)
+        jsonString.read(foo)
         jsonString.close()
+        val js = String(foo)
         val typeToken = object : TypeToken<List<Poster>>() {}.type
-        val poster = Gson().fromJson<List<Poster>>(jsonString, typeToken)
 
+        return Gson().fromJson(js, typeToken)
     }
 
+    fun posterBitMap(url: String) : Bitmap
+    {
+        val url = URL(url)
+        val bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream())
+        return bmp
+    }
 }
