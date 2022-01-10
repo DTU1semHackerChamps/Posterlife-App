efaac37dc2238e3ea6b6160bc4a6c736fc684df5
Stig <s186333@student.dtu.dk>
2022-01-10 14:16:48 +0100

    try photoeditor again

--- app/src/main/java/com/example/posterlifeapp/CameraActivity.kt
+++ app/src/main/java/com/example/posterlifeapp/CameraActivity.kt
@@ -87,7 +87,7 @@ fun MainContent(modifier: Modifier = Modifier){
                     .align(Alignment.BottomCenter)
                     .padding(16.dp),
                 onClick = {
-                    val intent = Intent(context,PhotoEditor::class.java)
+                    val intent = Intent(context,EditImageActivity::class.java)
                     intent.putExtra("imageUri", imageUri.toString())
                     context.startActivity(intent)
 
--- app/src/main/java/com/example/posterlifeapp/PhotoEditor.kt
+++ app/src/main/java/com/example/posterlifeapp/PhotoEditor.kt
@@ -1,29 +1,41 @@
 package com.example.posterlifeapp
 
+import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.Window
+import android.view.WindowManager
+import android.widget.ImageView
 import androidx.core.content.res.ResourcesCompat
 import ja.burhanrashid52.photoeditor.PhotoEditor
 import ja.burhanrashid52.photoeditor.PhotoEditorView
+import java.io.IOException
+
+var mPhotoEditor: PhotoEditor? = null
 
 class PhotoEditor : AppCompatActivity() {
-    lateinit var mPhotoEditor : PhotoEditor
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val mPhotoEditorView = findViewById<PhotoEditorView>(R.id.photoEditorView)
-        mPhotoEditorView.source.setImageResource(R.drawable.blank_image)
         setContentView(R.layout.activity_photo_editor)
+        val mPhotoEditorView = findViewById<PhotoEditorView>(R.id.photoEditorView)
+
+
+
+        //mPhotoEditorView.source.setImageResource(R.drawable.blank_image)
+
 
         // custom font
         val mTextRobotoTF : Typeface? = ResourcesCompat.getFont(this, R.font.roboto_medium)
 
         mPhotoEditor = PhotoEditor.Builder(this,mPhotoEditorView)
-            .setPinchTextScalable(true)
-            .setClipSourceImage(true)
-            .setDefaultTextTypeface(mTextRobotoTF)
+            //.setPinchTextScalable(true)
+            //.setClipSourceImage(true)
+            //.setDefaultTextTypeface(mTextRobotoTF)
             .build()
     }
 
