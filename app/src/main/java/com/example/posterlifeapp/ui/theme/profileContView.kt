86bd938718154b98bc52fa3d94888086816a8b5a
harzzek <39692510+harzzek@users.noreply.github.com>
2022-01-14 15:46:47 +0100

    Profil siden view ændring

--- app/src/main/java/com/example/posterlifeapp/ui/theme/profileContView.kt
+++ app/src/main/java/com/example/posterlifeapp/ui/theme/profileContView.kt
@@ -1,4 +1,4 @@
-package com.example.posterlifeapp.profileComposeables
+package com.example.posterlifeapp.ui.theme
 
 import android.os.Bundle
 import android.widget.Toast
@@ -17,7 +17,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.posterlifeapp.profileComposeables.ui.theme.PosterLifeAppTheme
+import com.example.posterlifeapp.ui.theme.PosterLifeAppTheme
 
 class profileContView {
 
